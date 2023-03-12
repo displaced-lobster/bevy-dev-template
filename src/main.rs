cfbc168639e10043b60d8207d65f65dadd56f450
Richard Mills <me@richardmills.ca>
2023-03-11 21:32:28 -0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use bevy::{input::common_conditions::input_toggle_active, prelude::*};
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(
+            WorldInspectorPlugin::default().run_if(input_toggle_active(true, KeyCode::Escape)),
+        )
+        .run();
+}
