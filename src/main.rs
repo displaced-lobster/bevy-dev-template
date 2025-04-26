a7eb0f6c40b19cb8ce634b002e8f38cceafe77d5
Richard Mills <me@richardmills.ca>
2025-04-26 11:25:57 -0700

    chore: upgrade bevy 0.16

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use bevy::{input::common_conditions::input_toggle_active, prelude::*};
+use bevy_egui::EguiPlugin;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin {
+            enable_multipass_for_primary_context: true,
+        })
         .add_plugins(
-            WorldInspectorPlugin::default().run_if(input_toggle_active(false, KeyCode::Escape)),
+            WorldInspectorPlugin::new().run_if(input_toggle_active(false, KeyCode::Escape)),
         )
         .run();
 }
