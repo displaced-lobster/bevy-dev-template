77a8d477f41cc37abcc879921cc88667108fff42
Richard Mills <me@richardmills.ca>
2025-09-07 09:28:03 -0700

    chore: upgrade egui

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,7 @@ use bevy_inspector_egui::quick::WorldInspectorPlugin;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(EguiPlugin {
-            enable_multipass_for_primary_context: true,
-        })
+        .add_plugins(EguiPlugin::default())
         .add_plugins(
             WorldInspectorPlugin::new().run_if(input_toggle_active(false, KeyCode::Escape)),
         )
